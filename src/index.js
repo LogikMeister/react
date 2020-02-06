b54cba33a1d1141ae0c4ea43b04a2a0449074096
LogikMeister <qbshuan@qq.com>
2020-02-06 17:00:41 +0800

    master

--- src/index.js
+++ src/index.js
@@ -1,18 +1,7 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import { Layout } from 'antd';
 
-const { Header, Footer, Sider, Content } = Layout;
-
-function App(){
-    return (
-        <Layout>
-            <Header>123</Header>
-            <Content>12334</Content>
-            <Footer>1236</Footer>
-        </Layout>
-    )
-}
+import App from './App.js'
 
 ReactDom.render(
     <App />,
